91d4639a37db2bd218ce4c0fd9ec341191d6ddb2
David Park <dspark410@gmail.com>
2022-12-19 19:50:08 -0800

    adds prices

--- client/src/components/Collection/Collection.tsx
+++ client/src/components/Collection/Collection.tsx
@@ -19,6 +19,70 @@ export const Collection = () => {
       {collection.map((card) => (
         <article key={card.id}>
           <Image src={card.images.large} />
+          <>
+            {card.tcgplayer?.prices.normal ? (
+              <section>
+                <span>Normal Prices</span>
+                <p>Low: {card.tcgplayer?.prices.normal?.low}</p>
+                <p>Mid: {card.tcgplayer?.prices.normal?.mid}</p>
+                <p>High: {card.tcgplayer?.prices.normal?.high}</p>
+                <p>Market:{card.tcgplayer?.prices.normal?.market}</p>
+              </section>
+            ) : null}
+            {card.tcgplayer?.prices.holofoil ? (
+              <section>
+                <span>Holofoil Prices</span>
+                <p>Low: {card.tcgplayer?.prices.holofoil?.low}</p>
+                <p>Mid: {card.tcgplayer?.prices.holofoil?.mid}</p>
+                <p>High: {card.tcgplayer?.prices.holofoil?.high}</p>
+                <p>Market: {card.tcgplayer?.prices.holofoil?.market}</p>
+              </section>
+            ) : null}
+            {card.tcgplayer?.prices.reverseHolofoil ? (
+              <section>
+                <span>Reverse Holofoil Prices</span>
+                <p>Low: {card.tcgplayer?.prices.reverseHolofoil?.low}</p>
+                <p>Mid: {card.tcgplayer?.prices.reverseHolofoil?.mid}</p>
+                <p>High: {card.tcgplayer?.prices.reverseHolofoil?.high}</p>
+                <p>Market: {card.tcgplayer?.prices.reverseHolofoil?.market}</p>
+              </section>
+            ) : null}
+            {card.tcgplayer?.prices['1stEditionNormal'] ? (
+              <section>
+                <span>1st Edition Normal Prices</span>
+                <p>Low: {card.tcgplayer?.prices['1stEditionNormal']?.low}</p>
+                <p>Mid: {card.tcgplayer?.prices['1stEditionNormal']?.mid}</p>
+                <p>High: {card.tcgplayer?.prices['1stEditionNormal']?.high}</p>
+                <p>
+                  Market: {card.tcgplayer?.prices['1stEditionNormal']?.market}
+                </p>
+              </section>
+            ) : null}
+            {card.tcgplayer?.prices['1stEditionHolofoil'] ? (
+              <section>
+                <span>1st Edition Holofoil Prices</span>
+                <p>Low: {card.tcgplayer?.prices['1stEditionHolofoil']?.low}</p>
+                <p>Mid: {card.tcgplayer?.prices['1stEditionHolofoil']?.mid}</p>
+                <p>
+                  High: {card.tcgplayer?.prices['1stEditionHolofoil']?.high}
+                </p>
+                <p>
+                  Market: {card.tcgplayer?.prices['1stEditionHolofoil']?.market}
+                </p>
+              </section>
+            ) : null}
+            {card.tcgplayer?.prices?.unlimitedHolofoil ? (
+              <section>
+                <span>Unlimited Holofoil Prices</span>
+                <p>Low: {card.tcgplayer?.prices?.unlimitedHolofoil?.low}</p>
+                <p>Mid: {card.tcgplayer?.prices?.unlimitedHolofoil?.mid}</p>
+                <p>High: {card.tcgplayer?.prices?.unlimitedHolofoil?.high}</p>
+                <p>
+                  Market: {card.tcgplayer?.prices?.unlimitedHolofoil?.market}
+                </p>
+              </section>
+            ) : null}
+          </>
         </article>
       ))}
     </main>
