6829a76438ac27052d8044d49343194eda079ad0
David Park <dspark410@gmail.com>
2022-12-29 11:58:24 -0800

    adds collection prices

--- client/src/components/Collection/Collection.tsx
+++ client/src/components/Collection/Collection.tsx
@@ -1,90 +1,48 @@
-import { useState, useEffect } from 'react'
-import { Image } from 'antd'
-import { PokemonInfo } from '../PokemonCardGrid/PokemonCard/pokemonCardTypes'
-import './collection.css'
+import { useState, useEffect } from "react";
+import { Image } from "antd";
+import { PokemonInfo } from "../PokemonCardGrid/PokemonCard/pokemonCardTypes";
+import "./collection.css";
 
 export const Collection = () => {
-  const [collection, setCollection] = useState<PokemonInfo[]>([])
+  const [collection, setCollection] = useState<PokemonInfo[]>([]);
+  const [price, setPrice] = useState(0);
+  const getCollection = localStorage.getItem("collection");
 
-  const getCollection = localStorage.getItem('collection')
+  const getCollectionPriceTotal = () => {
+    return collection.reduce((acc: any, card) => {
+      let cardPrice =
+        card?.tcgplayer?.prices.normal?.mid ||
+        card.tcgplayer?.prices.holofoil?.mid ||
+        card.tcgplayer?.prices.reverseHolofoil?.mid ||
+        card.tcgplayer?.prices["1stEditionNormal"]?.mid ||
+        card.tcgplayer?.prices["1stEditionHolofoil"]?.mid ||
+        card.tcgplayer?.prices?.unlimitedHolofoil?.mid;
+      return (acc += cardPrice);
+    }, 0);
+  };
 
   useEffect(() => {
     if (!!getCollection) {
-      setCollection(JSON.parse(getCollection))
+      setCollection(JSON.parse(getCollection));
     }
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    const collectionTotal = getCollectionPriceTotal();
+    console.log(collectionTotal);
+    setPrice(collectionTotal);
+  }, [collection]);
 
   return (
-    <main className='collection-grid-container'>
-      {collection.map((card) => (
-        <article key={card.id}>
-          <Image src={card.images.large} />
-          <>
-            {card.tcgplayer?.prices.normal ? (
-              <section>
-                <span>Normal Prices</span>
-                <p>Low: {card.tcgplayer?.prices.normal?.low}</p>
-                <p>Mid: {card.tcgplayer?.prices.normal?.mid}</p>
-                <p>High: {card.tcgplayer?.prices.normal?.high}</p>
-                <p>Market:{card.tcgplayer?.prices.normal?.market}</p>
-              </section>
-            ) : null}
-            {card.tcgplayer?.prices.holofoil ? (
-              <section>
-                <span>Holofoil Prices</span>
-                <p>Low: {card.tcgplayer?.prices.holofoil?.low}</p>
-                <p>Mid: {card.tcgplayer?.prices.holofoil?.mid}</p>
-                <p>High: {card.tcgplayer?.prices.holofoil?.high}</p>
-                <p>Market: {card.tcgplayer?.prices.holofoil?.market}</p>
-              </section>
-            ) : null}
-            {card.tcgplayer?.prices.reverseHolofoil ? (
-              <section>
-                <span>Reverse Holofoil Prices</span>
-                <p>Low: {card.tcgplayer?.prices.reverseHolofoil?.low}</p>
-                <p>Mid: {card.tcgplayer?.prices.reverseHolofoil?.mid}</p>
-                <p>High: {card.tcgplayer?.prices.reverseHolofoil?.high}</p>
-                <p>Market: {card.tcgplayer?.prices.reverseHolofoil?.market}</p>
-              </section>
-            ) : null}
-            {card.tcgplayer?.prices['1stEditionNormal'] ? (
-              <section>
-                <span>1st Edition Normal Prices</span>
-                <p>Low: {card.tcgplayer?.prices['1stEditionNormal']?.low}</p>
-                <p>Mid: {card.tcgplayer?.prices['1stEditionNormal']?.mid}</p>
-                <p>High: {card.tcgplayer?.prices['1stEditionNormal']?.high}</p>
-                <p>
-                  Market: {card.tcgplayer?.prices['1stEditionNormal']?.market}
-                </p>
-              </section>
-            ) : null}
-            {card.tcgplayer?.prices['1stEditionHolofoil'] ? (
-              <section>
-                <span>1st Edition Holofoil Prices</span>
-                <p>Low: {card.tcgplayer?.prices['1stEditionHolofoil']?.low}</p>
-                <p>Mid: {card.tcgplayer?.prices['1stEditionHolofoil']?.mid}</p>
-                <p>
-                  High: {card.tcgplayer?.prices['1stEditionHolofoil']?.high}
-                </p>
-                <p>
-                  Market: {card.tcgplayer?.prices['1stEditionHolofoil']?.market}
-                </p>
-              </section>
-            ) : null}
-            {card.tcgplayer?.prices?.unlimitedHolofoil ? (
-              <section>
-                <span>Unlimited Holofoil Prices</span>
-                <p>Low: {card.tcgplayer?.prices?.unlimitedHolofoil?.low}</p>
-                <p>Mid: {card.tcgplayer?.prices?.unlimitedHolofoil?.mid}</p>
-                <p>High: {card.tcgplayer?.prices?.unlimitedHolofoil?.high}</p>
-                <p>
-                  Market: {card.tcgplayer?.prices?.unlimitedHolofoil?.market}
-                </p>
-              </section>
-            ) : null}
-          </>
-        </article>
-      ))}
+    <main className="collection-container">
+      <section className="collection-grid-container">
+        {collection.map((card) => (
+          <article key={card.id}>
+            <Image src={card.images.large} />
+          </article>
+        ))}
+      </section>
+      <section className="price-container">{price}</section>
     </main>
-  )
-}
+  );
+};
